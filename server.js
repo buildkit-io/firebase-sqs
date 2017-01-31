7cfb52bc0d2c6cb6c6dcfb4aa1e7dbd11e741b00
Stefan Dimitrov <elwebmaster@users.noreply.github.com>
2017-01-31 04:57:34 +0000

    Fix key.

--- server.js
+++ server.js
@@ -2,8 +2,6 @@
 // Import Admin SDK
 var admin = require("firebase-admin");
 
-process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
-
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
