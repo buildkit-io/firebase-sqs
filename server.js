6fcb4b70eebbf29f460a6aa25f75d23519c557b6
Stefan Dimitrov <elwebmaster@users.noreply.github.com>
2017-02-01 03:42:08 +0000

    Add AWS.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //console.log(process.env.TIMES);
 // Import Admin SDK
 var admin = require("firebase-admin");
+var AWS = require('aws-sdk');
 
 admin.initializeApp({
     credential: admin.credential.cert({
@@ -15,9 +16,25 @@ admin.initializeApp({
 var db = admin.database();
 var ref = db.ref(process.env.FIREBASE_DB_REF);
 
+// SQS queue
+var sqs = new AWS.SQS({
+    apiVersion: '2012-11-05'
+});
+
 // Attach an asynchronous callback to read the data at our posts reference
 ref.on("value", function(snapshot) {
     console.log(snapshot.val());
+    sqs.sendMessage({
+        MessageBody: snapshot.val(),
+        QueueUrl: process.env.AWS_SQS_QUEUE_URL
+    }, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        }
+        else {
+            console.log("Success", data.MessageId);
+        }
+    });
 }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
